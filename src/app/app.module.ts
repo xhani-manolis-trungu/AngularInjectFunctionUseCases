b19c66e2397626dd717336c03be804be00055795
xhani-manolis-trungu <trmanolis@gmail.com>
2022-09-28 16:34:52 +0300

    remove unused imports from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './product.service';
 import { NgxsModule } from '@ngxs/store';
-import { NgxsLoggerPlugin } from '@ngxs/logger-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { AppState, AppStateModel } from './state/app.state';
+import { AppState } from './state/app.state';
 import { SearchComponent } from './search/search.component';
 import { UsersComponent } from './users/users.component';
-import { CommonModule } from '@angular/common';
-import { AngularMaterialModule } from './AngularMaterial/angular-material.module';
 
 @NgModule({
   imports: [
@@ -22,7 +19,7 @@ import { AngularMaterialModule } from './AngularMaterial/angular-material.module
     HttpClientModule,
     NgxsModule.forRoot([AppState]),
     NgxsLoggerPluginModule.forRoot({ disabled: false }),
-    UsersComponent
+    UsersComponent,
   ],
   declarations: [AppComponent, HelloComponent, SearchComponent],
   bootstrap: [AppComponent],
